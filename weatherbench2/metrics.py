ead9c2eb953421a68f99a142837bfaae97a76eb9
Genevieve Flaspohler <geflaspohler@gmail.com>
2024-10-31 14:03:41 -0700

    Spatial acc

--- weatherbench2/metrics.py
+++ weatherbench2/metrics.py
@@ -400,6 +400,42 @@ class ACC(Metric):
 
 
 @dataclasses.dataclass
+class SpatialACC(Metric):
+    """Spatial anomaly correlation coefficient.
+
+    Attribute:
+      climatology: Climatology for computing anomalies.
+    """
+
+    climatology: xr.Dataset
+
+    def compute_chunk(
+        self,
+        forecast: xr.Dataset,
+        truth: xr.Dataset,
+        region: t.Optional[Region] = None,
+    ) -> xr.Dataset:
+        if "init_time" in forecast.dims:
+            time_dim = "valid_time"
+        else:
+            time_dim = "time"
+        # Rename the lat and lon columns
+        if 'lon' in self.climatology and 'lat' in self.climatology:
+            self.climatology = self.climatology.rename(
+                {'lon': 'longitude', 'lat': 'latitude'})
+        climatology_chunk = _get_climatology_chunk(self.climatology, truth)
+        if hasattr(forecast, "level"):
+            climatology_chunk = climatology_chunk.sel(level=forecast.level)
+        time_selection = dict(dayofyear=forecast[time_dim].dt.dayofyear)
+        if "hour" in set(climatology_chunk.coords):
+            time_selection["hour"] = forecast[time_dim].dt.hour
+        climatology_chunk = climatology_chunk.sel(time_selection)
+        forecast_anom = forecast - climatology_chunk
+        truth_anom = truth - climatology_chunk
+        return forecast_anom * truth_anom / np.sqrt(forecast_anom**2 * truth_anom**2)
+
+
+@dataclasses.dataclass
 class SpatialSEEPS(Metric):
     """Computes Stable Equitable Error in Probability Space.
 
@@ -460,7 +496,8 @@ class SpatialSEEPS(Metric):
     ) -> xr.Dataset:
         # Rename the lat and lon columns
         if 'lon' in self.climatology and 'lat' in self.climatology:
-            self.climatology = self.climatology.rename({'lon': 'longitude', 'lat': 'latitude'})
+            self.climatology = self.climatology.rename(
+                {'lon': 'longitude', 'lat': 'latitude'})
 
         forecast_cat = self._convert_precip_to_seeps_cat(forecast)
         truth_cat = self._convert_precip_to_seeps_cat(truth)
